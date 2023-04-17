82778527dcb30529224b1c54f3abc763b0a1803b
Mahdi Bozorgi <102772767+Mehtisup@users.noreply.github.com>
2023-04-17 14:50:47 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function telFunction() {
+    window.open('https://t.me.com/mehtisup')
+}
+
+function insFunction() {
+    window.open('https://instagram.com/mehtisup')
+}
+
+function youFunction() {
+    window.open('https://youtube.com/mehtisup')
+}
